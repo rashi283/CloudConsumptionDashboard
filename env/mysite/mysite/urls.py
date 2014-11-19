cf31b4fc40d66d9eb191955edf4bafc1e36775e0
subhagini <subhagini.chaudhary@gmail.com>
2014-11-19 12:19:18 -0800

    chaged some files

--- env/mysite/mysite/urls.py
+++ env/mysite/mysite/urls.py
@@ -12,5 +12,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^registration/', 'myapp.views.registration', name='registration' ),
+    url(r'^consumer_Dashboard/', 'myapp.views.consumer_Dashboard', name='consumer_Dashboard' ),
     
 )
