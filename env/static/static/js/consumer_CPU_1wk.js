37a786fe47aeea98df42a3cff4591e454b31fb80
subhagini <subhagini.chaudhary@gmail.com>
2014-11-21 12:43:00 -0800

    updating js files

--- env/static/static/js/consumer_CPU_1wk.js
+++ env/static/static/js/consumer_CPU_1wk.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
-    $(".dropdown-menu li a").click(function() {
-        alert ("hi");
+  $(".dropdown-menu li a").click(function() {
   $(this).parents(".btn-group").find('.selection').text($(this).text());
   $(this).parents(".btn-group").find('.selection').val($(this).text());
 
     });
     
     
-    $("#1wk").click(function() {
+    $("#CPU_1wk").click(function() {
              options = {
                 chart: {
                     renderTo: 'CPU_Utilization',
                     type: 'line',
-                    marginRight: 130,
-                    marginBottom: 25
+                    marginRight: 100,
+                    marginBottom: 65
                 },
                 title: {
                     text: 'CPU Utilization',
@@ -23,20 +22,16 @@ $(document).ready(function() {
                     text: '',
                     x: -20
                 },
-                
-        
-                
-                
                xAxis: {
                     categories: [],
                     labels: {
-                                                step:2,
-                                         overflow: 'justify',
-					align: 'center',
+                                         step:2,
+                                       
+                                    rotation:-35,
 					x: -3,
 					y: 20,
 					formatter: function() {
-						return Highcharts.dateFormat('%e%b', Date.parse(this.value +' UTC'));
+						return Highcharts.dateFormat('%e%b :%l%p', Date.parse(this.value +' UTC'));
 					}
 				
                 }
@@ -51,6 +46,7 @@ $(document).ready(function() {
                         color: '#808080'
                     }]
                 },
+
                 tooltip: {
                     formatter: function() {
                             return '<b>'+ this.series.name +'</b><br/>'+
@@ -62,16 +58,26 @@ $(document).ready(function() {
                     align: 'right',
                     verticalAlign: 'top',
                     x: -10,
-                    y: 100,
+                    y: 50,
+                        floating: true,
                     borderWidth: 0
                 },
+                plotOptions: {
+                         series: {
+                                     marker: {
+                                        fillColor: '#FFFFFF',
+                                        lineWidth: 2,
+                                        lineColor: null // inherit from series
+                                                }
+                                    }
+                        },
                 series: [{}]
             };
             
             $.getJSON("/static/consumer_CPU_1wk.json", function(json) {
-               // alert(json['category']);
-               // alert(json['name']);
-                //alert(json['data']);
+               //alert(json['category']);
+               //alert(json['name']);
+              //alert(json['data']);
                 options.series[0].name = json['name'];
                 options.series[0].data = json['data'];
                 options.xAxis.categories = json['category'];
